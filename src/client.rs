bab0b45e0dc68a96b6e056a9f44ee65b1a6a9d18
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2025-01-16 07:08:19 +0530

    add(client): partical client implementation

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,25 @@
-
+use fastwebsockets::{WebSocket};
+use reqwest;
+use tokio::net::TcpStream;
+use tokio::sync::mpsc;
 
 pub struct Client {
+    local_addr: String,
+    remote_addr: String,
+    via_addr: String,
+}
 
+impl Client {
+    pub fn new(local_addr: String, remote_addr: String, via_addr: String) -> Client {
+        Client {
+            local_addr,
+            remote_addr,
+            via_addr,
+        }
+    }
+
+    /// proxies the request from local addr to websocket server
+    pub async fn listen_and_serve(&self, err_rx: mpsc::Receiver<WsmuxError>) {
+        let mut server_stream = TcpStream::connect(self.via_addr).await?;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,35 @@
 mod client;
 mod server;
-use clap::{Parser,Subcommand};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(
-    name ="wsmux",
-    about = "websocket based proxy",
-)]
+#[command(name = "wsmux", about = "websocket based proxy")]
 struct Cli {
     #[clap(subcommand)]
     command: Command,
 }
 
-#[derive(Subcommand,Debug)]
+#[derive(Subcommand, Debug)]
 enum Command {
     Serve {
-
-    }
+        #[arg(id = "addr", long, default_value = "0.0.0.0:8080")]
+        addr: String,
+    },
 
     Proxy {
+        #[arg(id = "server", long)]
+        server: String,
 
-    }
+        #[arg(id = "local-addr", long, default_value = "localhost")]
+        local_host: String,
+
+        #[arg(id = "remote-addr", long, default_value = "localhost")]
+        remote_host: String,
+
+        #[arg(id = "local-port", long)]
+        local_port: String,
+
+        #[arg(id = "remote-port", long)]
+        remote_port: String,
+    },
 }
